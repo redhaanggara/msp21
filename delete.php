3120c9599be9a15fbea8b2855af4d61d2062fbb2
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-05-04 18:03:29 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -11,7 +11,6 @@ function hapusData($id){
 	$usher= $_SESSION['pengguna'];
 	$query = "DELETE FROM databasegudang WHERE no='$id' AND username='$usher'";
 	echo $query;
-	$db = new mysqli("localhost", "root", "", "wk");
 	if(mysqli_query($db,$query) or die ('gagal')){
 		return true;
 	}
