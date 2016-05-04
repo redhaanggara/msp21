690cefa9201de4c54f1b1a597178589d891837b9
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-05-04 18:12:06 +0700

    Update acthutang.php

--- acthutang.php
+++ acthutang.php
@@ -8,14 +8,18 @@
 <a href=hutangform.php><h4 style="color:blue;">Back</h4></a><br>
 <?php
 session_start();
-include("db.php");
+$db = new mysqli("ap-cdbr-azure-southeast-b.cloudapp.net", "b5a0b7e6a5eda4", "d36febb7", "wk");
+if ($db->connect_errno){
+	
+	echo "error gan".$db->connect_error;
+}
 
 function tambahData($nmp,$tgl,$ket){
 	$usher= $_SESSION['pengguna'];
 	$query = "INSERT INTO databasehutang
 	(username,penghutang,date,ket)
 	VALUES ('$usher','$nmp','$tgl','$ket')";
-	$db = new mysqli("localhost", "root", "", "wk");
+	$db = new mysqli("ap-cdbr-azure-southeast-b.cloudapp.net", "b5a0b7e6a5eda4", "d36febb7", "wk");
 	if(mysqli_query($db,$query) or die ('gagal')){
 		return true;
 	}
