8ee4458b94e063954dacf0eda0a6f95b145264a8
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-05-04 17:12:00 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli("localhost", "root", "", "wk");
+$db = new mysqli("ap-cdbr-azure-southeast-b.cloudapp.net", "b5a0b7e6a5eda4", "d36febb7", "wk");
 if ($db->connect_errno){
 	
 	echo "error gan".$db->connect_error;
